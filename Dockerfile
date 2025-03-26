86129b8e55c0465a660165238607078805e3ec8e
Snehal <snehalbandi@kpmg.com>
2025-03-26 14:13:29 +0530

    added

---
+++ Dockerfile
@@ -0,0 +1,29 @@
+#
+# FROM tomcat:9.0-jdk17-temurin
+# COPY . /app
+# WORKDIR /app
+#
+# RUN ./mvnw package -DskipTest
+# RUN cp target/*.jar /usr/local/tomcat/webapps/ROOT.war
+# CMD ["catalina.sh", "run"]
+
+FROM openjdk:23-slim
+WORKDIR /app
+COPY target/docker-images-0.0.1-SNAPSHOT.jar
+EXPOSE 9099
+ENTRYPOINT ["java", "-jar", "docker-images-0.0.1-SNAPSHOT.jar"]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/main/java/com/example/docker_images/BookController.java
@@ -0,0 +1,26 @@
+package com.example.docker_images;
+
+
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BookController {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @GetMapping
+    public ResponseEntity<String> getUsers() {
+        String query = "SELECT COUNT(*) FROM books";
+        Integer count = jdbcTemplate.queryForObject(query, Integer.class);
+        return new ResponseEntity<>("Total books: " + count, HttpStatus.OK);
+    }
+}
